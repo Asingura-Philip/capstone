bb4f9eeb341f86c0994d436d9fbf6d6b26d54233
Asingura Philip <asinguraphilip@gmail.com>
2025-03-20 20:51:58 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const connection = require('./models/db')
 require('dotenv').config()
+
+const jobs = require('./routes/jobroutes')
+
+
+app.use('/',jobs)
 
 app.get('/',(req,res)=>{
     res.send('capstone')
 })
+
+
 
 
 
