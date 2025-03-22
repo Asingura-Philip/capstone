ac43502e77e2509f230b5806506eea0064fc6360
Asingura Philip <asinguraphilip@gmail.com>
2025-03-22 19:28:10 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
 const connection = require('./models/db')
 require('dotenv').config()
 
+
+
+
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
 const jobs = require('./routes/jobroutes')
+const auth = require('./routes/authroutes')
 
 
 app.use('/',jobs)
-
+app.use('/',auth)
 app.get('/',(req,res)=>{
     res.send('capstone')
 })
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
 
 
 
